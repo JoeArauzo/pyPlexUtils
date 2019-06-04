8766aecd1c66ef8958c12760ae9bc3706dff8e21
Joe Arauzo <joe@arauzo.net>
2019-06-03 18:13:44 -0700

    created basic package structure files and folders

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='pyPlexUtils',
+        version='0.0.1',
+        description='Includes useful command-line tools for managing media files for use with Plex Media Server.',
+        url='git@github.com:JoeArauzo/python-test-package.git',
+        author='Joe Arauzo',
+        author_email='joe@arauzo.net',
+        license='GPLv3+',
+        packages=['pyPlexUtils'],
+        zip_safe=False
+)
